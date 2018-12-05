9659e9000fcee6bd496b8283c74c58dd99adb48d
MattiaCarolo <mattia.carolo@studenti.unitn.it>
2018-12-05 19:13:57 +0100

    robe

--- app.test.js
+++ app.test.js
@@ -8,3 +8,17 @@ test('Trying to connect to the server', () => {
         .then(res => res.json())
         .then(json => expect(json.results).toEqual(9));
 });
+
+test('Wrong type', () => {
+
+	fetch('https://simulation2018.herokuapp.com/square?strdfing=1')
+        .then(res => res.json())
+        .then(json => expect(json.results).toEqual(-1));
+});
+
+test('No argument', () => {
+
+	fetch('https://simulation2018.herokuapp.com/square?string=')
+        .then(res => res.json())
+        .then(json => expect(json.results).toEqual(0));
+});
